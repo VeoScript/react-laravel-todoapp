9bbd492f0aa0803690def915d4441348e3774cd7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-05-31 19:55:46 +0800

    chore: update layout and design (#5)

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,21 +4,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
-const queryClient = new QueryClient();
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => require(`./Pages/${name}`),
     setup({ el, App, props }) {
-        return render(
-            <QueryClientProvider client={queryClient}>
-                <App {...props} />
-            </QueryClientProvider>
-        , el);
+        return render(<App {...props} />, el);
     },
 });
 
